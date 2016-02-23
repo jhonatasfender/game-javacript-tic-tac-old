fcbe9641eaefd6ccccf4124d7850bb92514871d1
jhonatasfender <jhonatas.fender@gmail.com>
2016-02-23 19:49:12 -0300

    estudos

--- example02.js
+++ example02.js
@@ -1,10 +1,14 @@
 (function () {
     function init(e) {
         var canvas = document.getElementsByTagName('canvas')[0],
-                c=canvas.getContext('2d'),caixa=canvas.getContext('2d');
-        c.fillStyle='black';
-        c.fillRect(0,0,canvas.width,canvas.height);
-        console.log(e);
+                c = canvas.getContext('2d'),
+                rect = canvas.getBoundingClientRect();
+        c.fillStyle = 'black';
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        console.log((e.clientX - rect.left) + '  ' + (e.clientY - rect.top));
+        c.font = "bold 10pt Courier";
+        c.fillStyle = 'white';
+        c.fillText("Hello world", 10, 10);
     }
     window.addEventListener('mousemove', init, false);
     window.addEventListener('click', init, false);
